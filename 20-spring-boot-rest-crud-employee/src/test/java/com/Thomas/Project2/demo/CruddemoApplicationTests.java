4ac7f95ba158e181fe8e21bffc881cc42b3de7b4
AnthomasGit <122173943+AnthomasGit@users.noreply.github.com>
2025-11-17 22:33:30 -0600

    Employee CRUD

---
+++ 20-spring-boot-rest-crud-employee/src/test/java/com/Thomas/Project2/demo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thomas.Project2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
